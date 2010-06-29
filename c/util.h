011c0289cf33c1dc796ac0dd78f4c3d172abbdfd
Andreas Kupries <akupries@shaw.ca>
2010-06-29 17:38:50 +0000

    Added protections against multiple inclusion, added math helper macros, and commentary

--- c/util.h
+++ c/util.h
@@ -1,14 +1,26 @@
+#ifndef CRIMP_UTIL_H
+#define CRIMP_UTIL_H
 /*
  * CRIMP :: Utility Declarations.
  * (C) 2010.
  */
 
-#define DUPSTR(str)    (char *) strcpy (ckalloc (strlen (str)+1), str)
-#define DUP(p,type)    (type *) memcpy (ALLOC (type), p, sizeof (type))
+/*
+ * Convenience macros for the allocation of structures and arrays.
+ */
 
-#define ALLOC(type)    (type *) ckalloc (sizeof (type))
-#define NALLOC(n,type) (type *) ckalloc ((n) * sizeof (type))
+#define DUPSTR(str)    ((char *) strcpy (ckalloc (strlen (str)+1), str))
+#define DUP(p,type)    ((type *) memcpy (ALLOC (type), p, sizeof (type)))
+#define ALLOC(type)    ((type *) ckalloc (sizeof (type)))
+#define NALLOC(n,type) ((type *) ckalloc ((n) * sizeof (type)))
 
+/*
+ * General math support.
+ */
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define CLAMP(min, v, max) ((v) < (min) ? (min) : (v) < (max) ? (v) : (max))
 
 /*
  * Local Variables:
@@ -17,3 +29,4 @@
  * fill-column: 78
  * End:
  */
+#endif /* CRIMP_UTIL_H */

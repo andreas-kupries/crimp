743caf94ebdbddd5f8c2cc1c4cfe4c88cf86b72f
Andreas Kupries <akupries@shaw.ca>
2010-06-30 04:26:32 +0000

    Added macros to support assertions

--- c/util.h
+++ c/util.h
@@ -28,6 +28,26 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define CLAMP(min, v, max) ((v) < (min) ? (min) : (v) < (max) ? (v) : (max))
+
+/*
+ * Assertions support in general, and asserting the proper range of an array
+ * index especially.
+ */
+
+#undef  CRIMP_DEBUG
+#define CRIMP_DEBUG 1
+
+#ifdef CRIMP_DEBUG
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
+#define RANGEOK(i,n) ((0 <= (i)) && (i < (n)))
+#define ASSERT(x,msg) if (!(x)) { Tcl_Panic (msg " (" #x "), in file " __FILE__ " @line " STR(__LINE__));}
+#define ASSERT_BOUNDS(i,n) ASSERT (RANGEOK(i,n),"array index out of bounds: " STR(i) " > " STR(n))
+#else
+#define ASSERT(x,msg)
+#define ASSERT_BOUNDS(i,n)
+#endif
+
 
 /*
  * Local Variables:

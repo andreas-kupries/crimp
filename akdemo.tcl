73bb68a5191160e4da6da8ce69f482c7e91292cf
Andreas Kupries <akupries@shaw.ca>
2010-06-30 20:57:33 +0000

    Added my own demo, showing off the implemented operations

---
+++ akdemo.tcl
@@ -0,0 +1,140 @@
+#!/bin/sh
+# -*- tcl -*-
+# The next line restarts with tclsh.\
+exec tclsh "$0" ${1+"$@"}
+
+package require Tcl 8.6 ; # Some code uses 8.6 bytearray features [New (NULL,len)].
+package require Tk  8.6 ; #
+#package require img::png
+#package require Tk
+package require widget::scrolledwindow
+package require widget::toolbar
+
+# Access to critcl library from a local unwrapped critcl app.
+set dir [file dirname [info script]]
+lappend auto_path [file join $dir critcl.vfs lib]
+
+# Direct access to the crimp package
+source [file join $dir crimp.tcl]
+
+# # ## ### ##### ######## #############
+##
+
+proc gui {} {
+    widget::toolbar .t
+    .t add button origin -text Original   -command sorigin
+    .t add button invert -text Invert     -command sinvert
+    .t add button red    -text Red        -command {schan 0}
+    .t add button green  -text Green      -command {schan 1}
+    .t add button blue   -text Blue       -command {schan 2}
+    .t add button ired   -text iRed       -command {sichan 0}
+    .t add button igreen -text iGreen     -command {sichan 1}
+    .t add button iblue  -text iBlue      -command {sichan 2}
+    .t add button ilum   -text Luminosity -command sluminosity
+    .t add button exit   -text Exit       -command ::exit -separator 1
+
+    widget::scrolledwindow .sl -borderwidth 1 -relief sunken
+    widget::scrolledwindow .sc -borderwidth 1 -relief sunken
+    listbox                .l -width 40 -selectmode single \
+	-listvariable images
+    canvas                 .c -width 800 -height 600 -scrollregion {-4000 -4000 4000 4000}
+
+    .c create image {0 0} -anchor nw -tags photo ;#-outline red
+    .c itemconfigure photo -image [image create photo]
+
+
+    .sl setwidget .l
+    .sc setwidget .c
+
+    pack .t  -fill both -expand 0 -side top -anchor w
+    pack .sl -fill both -expand 1 -padx 4 -pady 4 -side left
+    pack .sc -fill both -expand 1 -padx 4 -pady 4 -side right
+
+    bind .l <<ListboxSelect>> useSelection
+
+    # Panning via mouse
+    bind .c <ButtonPress-2> {%W scan mark   %x %y}
+    bind .c <B2-Motion>     {%W scan dragto %x %y}
+
+    # Cross hairs ...
+    #.c configure -cursor tcross
+    #crosshair::crosshair .c -width 0 -fill \#999999 -dash {.}
+    #crosshair::track on  .c TRACK
+
+    wm deiconify .
+    return
+}
+
+proc useSelection {} {
+    set selection [.l curselection]
+    if {![llength $selection]} return
+
+    show [.l get $selection]
+    return
+}
+
+proc show {name} {
+    global dir base
+
+    set photo [image create photo -file [file join $dir images $name]]
+    set base  [crimp read_tk $photo]
+    image delete $photo
+
+    sorigin
+    return
+}
+
+proc sorigin {} {
+    global base 
+    setimage $base
+    return
+}
+
+proc sinvert {} {
+    global base 
+    setimage [crimp invert_[typeof $base] $base]
+    return
+}
+
+proc sluminosity {} {
+    global base
+    setimage  [crimp convert_[typeof $base]_grey8 $base] 
+    return
+}
+
+proc schan  {idx} {
+    global base
+    setimage [lindex [crimp split_[typeof $base] $base] $idx]
+    return
+}
+
+proc sichan {idx} {
+    global base
+    set image [lindex [crimp split_[typeof $base] $base] $idx]
+    setimage [crimp invert_[typeof $image] $image]
+    return
+}
+
+proc setimage {i} {
+    #puts si/[join [crimp dimensions $i] x]
+    .c configure -scrollregion [list 0 0 {*}[crimp dimensions $i]]
+    crimp write_[typeof $i]_tk [.c itemcget photo -image] $i
+    return
+}
+
+proc typeof {image} {
+    return [namespace tail [crimp type $image]]
+}
+
+proc main {} {
+    global dir images
+    gui
+
+    set images [glob -tails -directory $dir/images *]
+    after 100 [list show [lindex $images 0]]
+    return
+}
+
+main
+vwait forever
+# vim: set sts=4 sw=4 tw=80 et ft=tcl:

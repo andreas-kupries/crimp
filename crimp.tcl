13a8030e8338e7e58c57fdb7ae96ff800df27f25
Andreas Kupries <akupries@shaw.ca>
2010-07-06 23:12:45 +0000

    Dropped the unused (any longer) parts from the old framework

--- crimp.tcl
+++ crimp.tcl
@@ -11,11 +11,6 @@
 #package require Tk
 package require critcl
 
-proc take {varname} {
-    upvar 1 $varname var
-    return $var[set var ""]
-}
-
 # # ## ### ##### ######## #############
 ## Implementation.
 
@@ -23,13 +18,16 @@ catch {
     critcl::cheaders -g
     critcl::debug memory symbols
 }
+
 critcl::config tk 1
 critcl::cheaders c/*.h
 critcl::csources c/*.c
 critcl::tsources crimp_tcl.tcl
+
 critcl::cinit {
     crimp_imagetype_init ();
 } {}
+
 critcl::ccode {
     #include <math.h>
     #include <stdlib.h>
@@ -38,44 +36,6 @@ critcl::ccode {
     #include <image.h>
     #include <color.h>
     #include <util.h>
-
-    static int decodeImageObj(Tcl_Interp *interp, Tcl_Obj *imageObj, int *width,
-    int *height, unsigned char **pixels)
-    {
-        int objc;
-        Tcl_Obj **objv;
-        if (Tcl_ListObjGetElements(interp, imageObj, &objc, &objv) != TCL_OK) {
-            return TCL_ERROR;
-        } else if (objc != 3) {
-            Tcl_SetResult(interp, "invalid image format", TCL_STATIC);
-            return TCL_ERROR;
-        } else if (Tcl_GetIntFromObj(interp, objv[0], width ) != TCL_OK
-                || Tcl_GetIntFromObj(interp, objv[1], height) != TCL_OK) {
-            return TCL_ERROR;
-        }
-
-        int length;
-        *pixels = Tcl_GetByteArrayFromObj(objv[2], &length);
-        if (length != 4 * *width * *height || *width < 0 || *height < 0) {
-            Tcl_SetResult(interp, "invalid image format", TCL_STATIC);
-            return TCL_ERROR;
-        }
-        return TCL_OK;
-    }
-
-    static int getUnsharedImageObj(Tcl_Interp *interp, Tcl_Obj *inputObj,
-    Tcl_Obj **outputObj, Tcl_Obj **dataObj)
-    {
-        *outputObj = inputObj;
-        if (Tcl_ListObjIndex(interp, *outputObj, 2, dataObj) != TCL_OK) {
-            return TCL_ERROR;
-        } else if (Tcl_IsShared(*outputObj) || Tcl_IsShared(*dataObj)) {
-            *outputObj = Tcl_DuplicateObj(*outputObj);
-            *dataObj = Tcl_DuplicateObj(*dataObj);
-            Tcl_ListObjReplace(interp, *outputObj, 2, 1, 1, dataObj);
-        }
-        return TCL_OK;
-    }
 }
 
 # # ## ### ##### ######## #############

9b96945dfb078b8e10e66d474d1bb16bc7e01275
Andreas Kupries <akupries@shaw.ca>
2010-07-12 21:59:56 +0000

    <verbatim> Extended the histogram demo.
    (1) Compute cumulative density functions for RGB, luma, and SV of HSV.
    (2) Show the various CDF in plots.
    (3) Added histogram equalization (stretching) in RGB and HSV spaces (SV actually).
        Playing around shows that it might be sensible to have only S or V
        stretching in HSV.
    
    The equalization in RGB shows how this naive operation (i.e. the
    separate handling of the color channels) causes shifts in hue.
    Equalization of the V generally makes the picture darker.
    
    Oh, right, I seem to remember reading that for this stuff we have to
    take out the gamma first, then equalize, and at last put the gamma
    back in. I might try that as another demo.
    
    
    Side problems with the plots, talk to Arjen:
    
    	No control over the size of plotcharts. The histogram demo has
    	so many plots now that the toplevel doesn't fit completely on
    	the screen. Either the plots get smaller, or I (have to) split
    	the demo by color space.
    
    	After equalization we seem to have lots of zero counts around
    	for intermediate values. The nature of the xyplot (linear
    	lines between values) causes a moire which repeats the main
    	plot at about half height. That looks ugly. Might have to use
    	a different plot type to get rid of this. Barchart should
    	work, except that it fills the area under the curve, which I
    	don't want.

--- demos/histogram.tcl
+++ demos/histogram.tcl
@@ -1,38 +1,143 @@
 def effect_histogram {
     label Histogram
     setup {
+	set ::TR {0 1}
+	set ::TG {0 1}
+	set ::TB {0 1}
+	set ::TS {0 1}
+	set ::TV {0 1}
+	set ::TL {0 1}
 
-	array set TMP [crimp histogram [base]]
-	array set TMP [crimp histogram [crimp convert 2grey8 [base]]]
-	array set TMP [crimp histogram [crimp convert 2hsv   [base]]]
-	set ::HR [dict values $TMP(red)]
-	set ::HG [dict values $TMP(green)]
-	set ::HB [dict values $TMP(blue)]
-	set ::HL [dict values $TMP(luma)]
-	set ::HH [dict values $TMP(hue)]
-	set ::HS [dict values $TMP(saturation)]
-	set ::HV [dict values $TMP(value)]
+	proc HISTO {image} {
+	    global HR HG HB HL HH HS HV
+	    global TR TG TB TL    TS TV
+
+	    array set TMP [crimp histogram $image]
+	    array set TMP [crimp histogram [crimp convert 2grey8 $image]]
+	    array set TMP [crimp histogram [crimp convert 2hsv   $image]]
+
+	    set HR [dict values $TMP(red)]   ; set TR [CUMULATE $HR]
+	    set HG [dict values $TMP(green)] ; set TG [CUMULATE $HG]
+	    set HB [dict values $TMP(blue)]  ; set TB [CUMULATE $HB]
+
+	    set HL [dict values $TMP(luma)]  ; set TL [CUMULATE $HL]
+
+	    set HH [dict values $TMP(hue)]
+	    set HS [dict values $TMP(saturation)] ; set TS [CUMULATE $HS]
+	    set HV [dict values $TMP(value)]      ; set TV [CUMULATE $HV]
+
+	    # For display only, kill the two most probable outliers at
+	    # the ends of each series/plot.
+	    
+	    lset HR 0 0 ; lset HR 255 0
+	    lset HG 0 0 ; lset HG 255 0
+	    lset HB 0 0 ; lset HB 255 0
+	    lset HL 0 0 ; lset HL 255 0
+
+	    lset HS 0 0 ; lset HS 255 0
+	    lset HV 0 0 ; lset HV 255 0
+	    return
+	}
+
+	proc EQNONE {} {
+	    HISTO [base]
+	    show_image [base]
+	}
+
+	proc EQHSV {} {
+	    HISTO [base]
+	    # H is not stretched. Does not make sense for HUE.
+	    global HH HS HV TS TV
+
+	    set fs [FIT $TS 255]
+	    set fv [FIT $TV 255]
+
+	    set h [crimp map identity]
+	    #set s [crimp map identity]
+	    #set v [crimp map identity]
+	    set s [crimp read tcl [list $fs]]
+	    set v [crimp read tcl [list $fv]]
+
+	    set new [crimp convert 2rgb \
+			 [crimp remap \
+			      [crimp convert 2hsv [base]] \
+			      $h $s $v]]
+
+	    show_image $new
+	    HISTO $new
+	    return
+	}
+
+	proc EQRGB {} {
+	    HISTO [base]
+	    global HR HG HB TR TG TB
+
+	    set fr [FIT $TR 255]
+	    set fg [FIT $TG 255]
+	    set fb [FIT $TB 255]
+
+	    set r [crimp read tcl [list $fr]]
+	    set g [crimp read tcl [list $fg]]
+	    set b [crimp read tcl [list $fb]]
+
+	    set new [crimp remap [base] $r $g $b]
+
+	    show_image $new
+	    HISTO $new
+	    return
+	}
+
+	# series(int) --> series (int)
+	proc CUMULATE {series} {
+	    set res {}
+	    set sum 0
+	    foreach x $series {
+		lappend res $sum
+		incr sum $x
+	    }
+	    return $res
+	}
+
+	# series(int/float) --> series(int), all(x): x <= max
+	proc FIT {series max} {
+	    # Assumes that the input is a monotonically increasing
+	    # series. The maximum value of the series is at the end.
+	    set top [lindex $series end]
+	    set f   [expr {double($max) / double($top)}]
+	    set res {}
+
+	    foreach x $series {
+		lappend res [expr {round(double($x)*$f)}]
+	    }
+	    return $res
+	}
+
+	HISTO [base]
 
 	# For the sake of the display we cut out the pure white and
 	# black, as they are likely outliers with an extreme number of
 	# pixels using them.
-
-	lset ::HR 0 0 ; lset ::HR 255 0
-	lset ::HG 0 0 ; lset ::HG 255 0
-	lset ::HB 0 0 ; lset ::HB 255 0
-	lset ::HL 0 0 ; lset ::HL 255 0
-
-	lset ::HS 0 0 ; lset ::HS 255 0
-	lset ::HV 0 0 ; lset ::HV 255 0
 	
-	plot  .left.hr -variable ::HR -locked 0
-	plot  .left.hg -variable ::HG -locked 0
-	plot  .left.hb -variable ::HB -locked 0
+	plot  .left.hr -variable ::HR -locked 0 -title Red
+	plot  .left.hg -variable ::HG -locked 0 -title Green
+	plot  .left.hb -variable ::HB -locked 0 -title Blue
 
-	plot  .top.hl -variable ::HL -locked 0
-	plot  .top.hh -variable ::HH -locked 0
-	plot  .top.hs -variable ::HS -locked 0
-	plot  .top.hv -variable ::HV -locked 0
+	plot  .top.hl -variable ::HL -locked 0 -title Luma
+	plot  .top.hh -variable ::HH -locked 0 -title Hue
+	plot  .top.hs -variable ::HS -locked 0 -title Saturation
+	plot  .top.hv -variable ::HV -locked 0 -title Value
+
+	ttk::button .right.eqhsv  -text {Equalize HSV}  -command EQHSV
+	ttk::button .right.eqrgb  -text {Equalize RGB}  -command EQRGB
+	ttk::button .right.eqnone -text {Equalize None} -command EQNONE
+
+	plot  .right.tr -variable ::TR -locked 0 -title {CDF Red}
+	plot  .right.tg -variable ::TG -locked 0 -title {CDF Green}
+	plot  .right.tb -variable ::TB -locked 0 -title {CDF Blue}
+	plot  .top.tl   -variable ::TL -locked 0 -title {CDF Luma}
+	plot  .top.ts   -variable ::TS -locked 0 -title {CDF Saturation}
+	plot  .top.tv   -variable ::TV -locked 0 -title {CDF Value}
+
 
 	grid .left.hr -row 0 -column 0 -sticky swen
 	grid .left.hg -row 1 -column 0 -sticky swen
@@ -42,8 +147,24 @@ def effect_histogram {
 	grid .top.hh -row 0 -column 1 -sticky swen
 	grid .top.hs -row 0 -column 2 -sticky swen
 	grid .top.hv -row 0 -column 3 -sticky swen
+
+	grid .right.eqrgb  -row 0 -column 0 -sticky swen
+	grid .right.eqhsv  -row 1 -column 0 -sticky swen
+	grid .right.eqnone -row 2 -column 0 -sticky swen
+
+	grid .right.tr -row 3 -column 0 -sticky swen
+	grid .right.tg -row 4 -column 0 -sticky swen
+	grid .right.tb -row 5 -column 0 -sticky swen
+
+	grid .top.tl -row 1 -column 0 -sticky swen
+	grid .top.ts -row 1 -column 2 -sticky swen
+	grid .top.tv -row 1 -column 3 -sticky swen
+
     }
     shutdown {
-	unset ::HR ::HG ::HB ::HL ::HH ::HS ::HV
+	rename HISTO    {} ; rename EQRGB  {}
+	rename FIT      {} ; rename EQHSV  {}
+	rename CUMULATE {} ; rename EQNONE {}
+	unset ::HR ::HG ::HB ::HL ::HH ::HS ::HV ::TR ::TG ::TB ::TS ::TV ::TL
     }
 }

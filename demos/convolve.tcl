2b954a83cf1972c852a4d3224960b40b958b317a
Andreas Kupries <akupries@shaw.ca>
2010-07-20 03:29:57 +0000

    Demos. Factored kernel translation out of the convolve method, added kernel transposition. Updated demo for API change, caching the translated kernel. Added demo, box blur, demonstrating speed advantage for separable kernels.

--- demos/convolve.tcl
+++ demos/convolve.tcl
@@ -1,12 +1,14 @@
-def op_convolve {
-    label Convolve
+def op_convolve_gaussian {
+    label {Gaussian Blur}
+    setup {
+	variable K [crimp kernel {
+	    {2  4  5  4 2}
+	    {4  9 12  9 4}
+	    {5 12 15 12 5}
+	    {4  9 12  9 4}
+	    {2  4  5  4 2}}]
+    }
     setup_image {
-	show_image [crimp convolve \
-			[base] \
-			{   {2  4  5  4 2}
-			    {4  9 12  9 4}
-			    {5 12 15 12 5}
-			    {4  9 12  9 4}
-			    {2  4  5  4 2}}]
+	show_image [crimp convolve [base] $K]
     }
 }

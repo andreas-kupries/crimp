0b18a2a537d4852d483a71ec8a6bf8f7a44d4beb
Andreas Kupries <akupries@shaw.ca>
2010-07-30 05:07:48 +0000

    Demos. Updates the demos performing their slideshows to use the new support in the framework instead. This simplified them substantially.

--- demos/pyramid_gauss.tcl
+++ demos/pyramid_gauss.tcl
@@ -1,35 +1,9 @@
 def op_pyramid_gauss {
     label {Gauss pyramid}
     setup_image {
-	# Create a gaussian image pyramid.
+	# Create a gaussian image pyramid and cycle through the
+	# results.
 
-	variable images
-	set images [crimp pyramid gauss [base] 4]
-    }
-    setup {
-	variable token
-
-	proc cycle {lv} {
-	    upvar 1 $lv list
-	    set tail [lassign $list head]
-	    set list [list {*}$tail $head]
-	    return $head
-	}
-
-	proc next {} {
-	    variable token
-	    set token [after 1000 DEMO::next]
-
-	    variable images
-	    if {![info exists images] || ![llength $images]} return
-
-	    show_image [cycle images]
-	    return
-	}
-
-	next
-    }
-    shutdown {
-	after cancel $token
+	show_slides [crimp pyramid gauss [base] 4]
     }
 }
--- demos/rof_median.tcl
+++ demos/rof_median.tcl
@@ -4,38 +4,10 @@ def op_rof_median {
 	# Create a series of median-filtered images from the base,
 	# with different kernel radii.
 
-	variable images
-	set images {}
-
-	lappend images [base]
-	lappend images [crimp rankfilter [base]]
-	lappend images [crimp rankfilter [base] 10]
-	lappend images [crimp rankfilter [base] 20]
-    }
-    setup {
-	variable token
-
-	proc cycle {lv} {
-	    upvar 1 $lv list
-	    set tail [lassign $list head]
-	    set list [list {*}$tail $head]
-	    return $head
-	}
-
-	proc next {} {
-	    variable token
-	    set token [after 1000 DEMO::next]
-
-	    variable images
-	    if {![info exists images] || ![llength $images]} return
-
-	    show_image [cycle images]
-	    return
-	}
-
-	next
-    }
-    shutdown {
-	after cancel $token
+	show_slides [list \
+			 [base] \
+			 [crimp rankfilter [base]] \
+			 [crimp rankfilter [base] 10] \
+			 [crimp rankfilter [base] 20]]
     }
 }

0a90bf644283250b71663f22e9f8722a687351e5
Andreas Kupries <akupries@shaw.ca>
2010-08-04 19:50:38 +0000

    Documentation. Helper script added to generate the images and embedded docs. The helper requires mimetex, ImageMagick's convert, and tklib's dia application with attendant packages, especially img::png and its dependencies

---
+++ tools/makedoc.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# tools
+# % generate the embedded documentation.
+# % the images required by it as well.
+
+# 1. math formulas               -> png	/requires mimetex + imagemagick convert.
+# 2. tklib/diagram based figures -> png	/requires tklib dia application.
+# 3. html + images from the doctools
+# 4. nroff from the doctools
+
+(   cd doc
+    (   cd figures
+	(   cd math
+	    echo ___ MATH _________
+	    rm -rf *.png
+	    for figure in *.txt ; do
+		echo $figure
+
+		mimetex -f $figure -e $$.gif
+		convert $$.gif $(basename $figure .txt).png
+		rm $$.gif
+	    done
+	)
+	echo ___ DIA _________
+	dia convert -t -o . png *.dia
+    )
+
+    echo ___ MAN _________
+    rm -rf     ../embedded/man
+    mkdir      ../embedded/man
+    dtplite -ext n -o ../embedded/man nroff .
+
+    echo ___ WWW _________
+    rm -rf     ../embedded/www
+    mkdir      ../embedded/www
+    dtplite -o ../embedded/www html  .
+)
+
+echo ___ MAN /show _________
+less embedded/man/files/crimp.n
+
+exit

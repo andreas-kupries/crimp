4e714746710311a1eb78dce5b39a702fad55b3e8
Andreas Kupries <akupries@shaw.ca>
2010-08-26 20:07:21 +0000

    Added 'table eval' method converting a function (command prefix) into a table for remapping. Additionally added method to compose tables, i.e. compute the result of chaining their application into a single table.
    Fixed ticket [7dea5be1bf9f099bfa46614ba5992c51245a8805].

---
+++ demos/chain_gauss_solarized.tcl
@@ -0,0 +1,56 @@
+def op_gauss_sol {
+    label {(Solarize o Gauss) Map}
+    setup {
+	variable sigma      42
+	variable threshold 256
+
+	variable gtable     {}
+	variable stable     {}
+	variable ctable     {}
+
+	proc showg {thesigma} {
+	    variable gtable [crimp table gauss $thesigma]
+	    showit
+	    return
+	}
+
+	proc shows {thethreshold} {
+	    variable stable [crimp table solarize $thethreshold]
+	    showit
+	    return
+	}
+
+	proc showit {} {
+	    variable gtable
+	    variable stable
+	    variable ctable
+
+	    # Block the early calls with incompletely initialized tables.
+	    if {![llength $gtable]} return
+	    if {![llength $stable]} return
+
+	    # Compose and apply.
+	    set ctable [crimp table compose $stable $gtable]
+	    show_image [crimp remap [base] [crimp mapof $ctable]]
+	    return
+	}
+
+	plot  .left.pg -variable ::DEMO::gtable -title Sigma
+	plot  .left.ps -variable ::DEMO::stable -title Threshold
+	plot  .left.pc -variable ::DEMO::ctable -title Composition
+
+	scale .left.sg -variable ::DEMO::sigma     -from 0.1 -to 150 -resolution 0.1 -orient horizontal -command ::DEMO::showg
+	scale .left.ss -variable ::DEMO::threshold -from 0   -to 256                 -orient horizontal -command ::DEMO::shows
+
+	grid .left.sg -row 0 -column 0 -sticky swen
+	grid .left.pg -row 1 -column 0 -sticky swen
+
+	grid .left.ss -row 2 -column 0 -sticky swen
+	grid .left.ps -row 3 -column 0 -sticky swen
+
+	grid .left.pc -row 4 -column 0 -sticky swen
+    }
+    setup_image {
+	showit
+    }
+}

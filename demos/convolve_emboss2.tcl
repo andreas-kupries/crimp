d0f8a5ebfb8c99a698f099b8de9099467724305c
Andreas Kupries <akupries@shaw.ca>
2010-09-02 22:39:44 +0000

    More convolutions (wiki ulis), plus better naming and referencing of origins

--- demos/convolve_emboss2.tcl
+++ demos/convolve_emboss2.tcl
@@ -1,11 +1,11 @@
-def op_convolve_emboss {
-    label {Emboss (Ulis)}
+def op_convolve_embossb {
+    label {Emboss (Suchenwirth)}
     setup {
-	# http://wiki.tcl.tk/10543
+	# http://wiki.tcl.tk/9521
 	variable K [crimp kernel make {
-	    {-1 -1 1}
-	    {-1 -1 1}
-	    { 1  1 1}}]
+	    {2  0  0}
+	    {0 -1  0}
+	    {0  0 -1}}]
     }
     setup_image {
 	show_image [crimp alpha opaque [crimp filter convolve [base] $K]]

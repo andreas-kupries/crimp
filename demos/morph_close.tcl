0a42470a95becc72d831a5eb1090e290c2419bc3
Andreas Kupries <akupries@shaw.ca>
2010-09-09 01:07:00 +0000

    Force a proper alpha channel on the basic morphology demos.

--- demos/morph_close.tcl
+++ demos/morph_close.tcl
@@ -1,6 +1,6 @@
 def op_morph_close {
     label {Morphology: Close}
     setup_image {
-	show_image [crimp morph close [base]]
+	show_image [crimp alpha opaque [crimp morph close [base]]]
     }
 }
--- demos/morph_dilate.tcl
+++ demos/morph_dilate.tcl
@@ -1,6 +1,6 @@
 def op_morph_dilate {
     label {Morphology: Dilate}
     setup_image {
-	show_image [crimp morph dilate [base]]
+	show_image [crimp alpha opaque [crimp morph dilate [base]]]
     }
 }

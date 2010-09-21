ec5c112e3d5cff262c93547013aa0c8a5e41c1e7
Andreas Kupries <akupries@shaw.ca>
2010-09-21 08:02:26 +0000

    Demos. Additional laplace/sobel variants.

--- demos/convolve_laplace8.tcl
+++ demos/convolve_laplace8.tcl
@@ -1,10 +1,10 @@
-def op_convolve_laplace9 {
-    label {Laplace 8+1}
+def op_convolve_laplace8 {
+    label {Laplace 8}
     setup {
 	# http://wiki.tcl.tk/9521
 	variable K [crimp kernel make {
 	    {-1  -1 -1}
-	    {-1   9 -1}
+	    {-1   8 -1}
 	    {-1  -1 -1}}]
     }
     setup_image {

88778d117810266bf22f7be7e77285325bd35464
Andreas Kupries <akupries@shaw.ca>
2010-09-21 07:17:45 +0000

    Redirect statistics into the new log window

--- demos/statistics.tcl
+++ demos/statistics.tcl
@@ -1,7 +1,20 @@
 def op_statistics {
     label Statistics
     setup_image {
+	variable stat
+	variable hist
 	array set stat [crimp statistics [base]]
-	parray    stat
+
+	foreach k [lsort -dict [array names stat]] {
+	    if {[string match {channel *} $k]} {
+		array set hist $stat($k)
+		log "$k"
+		foreach j [lsort -dict [array names hist]] {
+		    log "\t$j = $hist($j)"
+		}
+	    } else {
+		log "$k = $stat($k)"
+	    }
+	}
     }
 }

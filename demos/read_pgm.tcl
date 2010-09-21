af59c6d1273a0caed6012a3ea5bfa11b58b2687b
Andreas Kupries <akupries@shaw.ca>
2010-09-21 07:10:38 +0000

    Demos. Image Readers. Moved to be active if nothing is selected, like psych

--- demos/read_pgm.tcl
+++ demos/read_pgm.tcl
@@ -1,5 +1,8 @@
 def read_pgm {
     label {Read (PGM)}
+    active {
+	expr {[bases] == 0}
+    }
     setup {
 	show_image [crimp read pgm [fileutil::cat $dir/images/feep.pgm]]
     }
--- demos/read_ppm.tcl
+++ demos/read_ppm.tcl
@@ -1,5 +1,8 @@
 def read_ppm {
     label {Read (PPM)}
+    active {
+	expr {[bases] == 0}
+    }
     setup {
 	show_image [crimp read ppm [fileutil::cat $dir/images/blink.ppm]]
     }

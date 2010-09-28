e2e8ea93c66bc2a86ad86196fbb061327215cc03
Andreas Kupries <akupries@shaw.ca>
2010-09-28 04:39:50 +0000

    Blank demo extended to color choice

--- demos/blank.tcl
+++ demos/blank.tcl
@@ -4,6 +4,36 @@ def op_blank {
 	expr {[bases] == 0}
     }
     setup {
-	show_image [crimp blank rgba 800 600 0 0 0 255]
+	variable r 0
+	variable g 0
+	variable b 0
+
+	proc show {args} {
+	    variable r
+	    variable g
+	    variable b
+
+	    show_image [crimp blank rgba 800 600 $r $g $b 255]
+	    return
+	}
+
+	scale .left.r -variable ::DEMO::r \
+	    -from 0 -to 255 \
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	scale .left.g -variable ::DEMO::g \
+	    -from 0 -to 255 \
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	scale .left.b -variable ::DEMO::b \
+	    -from 0 -to 255 \
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	grid .left.r -row 0 -column 0 -sticky swen
+	grid .left.g -row 0 -column 1 -sticky swen
+	grid .left.b -row 0 -column 2 -sticky swen
     }
 }

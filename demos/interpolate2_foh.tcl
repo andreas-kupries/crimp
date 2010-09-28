47d0be53715d0053bb0fb65b5e140234c5b6074d
Andreas Kupries <akupries@shaw.ca>
2010-09-28 04:46:04 +0000

    Fix the replication kernels demonstrating 1st-order-hold

--- demos/interpolate2_foh.tcl
+++ demos/interpolate2_foh.tcl
@@ -2,7 +2,7 @@ def op_up_interpolate2foh {
     label Interpolate\u21912/FOH
     setup {
 	# First order hold kernel.
-	set K [crimp kernel make {{1 1 0}} 1]
+	set K [crimp kernel make {{0 1 1}} 1]
     }
     setup_image {
 	show_image [crimp alpha opaque [crimp interpolate [base] 2 $K]]
--- demos/interpolate8_foh.tcl
+++ demos/interpolate8_foh.tcl
@@ -2,7 +2,7 @@ def op_up_interpolate8foh {
     label Interpolate\u21918/FOH
     setup {
 	# Tent kernel.
-	set K [crimp kernel make {{1 1 0}} 1]
+	set K [crimp kernel make {{0 1 1}} 1]
     }
     setup_image {
 	show_image [crimp alpha opaque \

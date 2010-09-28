d5612b4a40f196a6e0f0eb5ddf6b5e56a869379e
Andreas Kupries <akupries@shaw.ca>
2010-09-28 03:39:50 +0000

    Demos of interpolation with first-order-hold = pixel replication

---
+++ demos/interpolate8_foh.tcl
@@ -0,0 +1,16 @@
+def op_up_interpolate8foh {
+    label Interpolate\u21918/FOH
+    setup {
+	# Tent kernel.
+	set K [crimp kernel make {{1 1 0}} 1]
+    }
+    setup_image {
+	show_image [crimp alpha opaque \
+			[crimp interpolate \
+			     [crimp interpolate \
+				  [crimp interpolate [base] \
+				       2 $K] \
+				  2 $K] \
+			     2 $K]]
+    }
+}

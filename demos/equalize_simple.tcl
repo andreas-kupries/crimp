77e7f29383ac7f0d68afe91e0dd1666d86d1ea3b
Andreas Kupries <akupries@shaw.ca>
2010-10-19 01:32:14 +0000

    Extended the "simple equalization" demo with AHE

--- demos/equalize_simple.tcl
+++ demos/equalize_simple.tcl
@@ -117,12 +117,23 @@ def effect_equalize_simple {
 	    return
 	}
 
+	proc EQUAL_AHE {} {
+	    demo_time_hook ahe {
+		show_image [crimp convert 2rgb \
+				[crimp filter ahe \
+				     [crimp convert 2hsv [base]] \
+				     100]]
+	    }
+	    return
+	}
+
 	ttk::button .top.plain   -text Plain       -command ::DEMO::PLAIN
 	ttk::button .top.equallu -text Eq/Luma     -command ::DEMO::EQUAL_LUMA
 	ttk::button .top.equalr2 -text Eq/RGB/Luma -command ::DEMO::EQUAL_RGB2
 	ttk::button .top.equalrg -text Eq/RGB      -command ::DEMO::EQUAL_RGB
 	ttk::button .top.equalsv -text Eq/SV       -command ::DEMO::EQUAL_SV
 	ttk::button .top.equalvv -text Eq/V        -command ::DEMO::EQUAL_V
+	ttk::button .top.equalah -text Eq/AHE      -command ::DEMO::EQUAL_AHE
 
 	grid .top.plain   -row 0 -column 0 -sticky swen
 	grid .top.equallu -row 0 -column 1 -sticky swen
@@ -130,5 +141,6 @@ def effect_equalize_simple {
 	grid .top.equalrg -row 0 -column 3 -sticky swen
 	grid .top.equalsv -row 0 -column 4 -sticky swen
 	grid .top.equalvv -row 0 -column 5 -sticky swen
+	grid .top.equalah -row 0 -column 6 -sticky swen
     }
 }

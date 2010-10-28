718c0802980fda104ae992b4e4d19a0cae9b77ee
Andreas Kupries <akupries@shaw.ca>
2010-10-28 05:57:01 +0000

    Demos. Threshold. Started experimentation on methods for the automatic calculation of a global threshold

--- demos/threshold_le.tcl
+++ demos/threshold_le.tcl
@@ -21,14 +21,48 @@ def op_threshold_le {
 	    return
 	}
 
+	# Demo various ways of for the automatic calculation of a
+	# global threshold.
+	proc average {} {
+	    variable threshold
+	    array set s [crimp statistics [crimp convert 2grey8 [base]]]
+	    array set s ${s(channel luma)}
+	    set threshold $s(mean)
+	    showit
+	    return
+	}
+
+	proc median {} {
+	    variable threshold
+	    array set s [crimp statistics [crimp convert 2grey8 [base]]]
+	    array set s ${s(channel luma)}
+	    set threshold $s(median)
+	    showit
+	    return
+	}
+
+	proc k-means {} {
+	    variable threshold
+	    array set s [crimp statistics [crimp convert 2grey8 [base]]]
+	    array set s ${s(channel luma)}
+	    # TODO ... classify $s(histogram) 0 255 ...
+	    showit
+	    return
+	}
+
 	plot  .left.p -variable ::DEMO::table -title Threshold
 	scale .left.s -variable ::DEMO::threshold \
 	    -from -1 -to 256 \
 	    -orient horizontal \
 	    -command ::DEMO::show
 
-	grid .left.s -row 0 -column 0 -sticky swen
-	grid .left.p -row 1 -column 0 -sticky swen
+	button .left.avg -text Average -command ::DEMO::average
+	button .left.med -text Median  -command ::DEMO::median
+
+	grid .left.s   -row 0 -column 0 -sticky swen
+	grid .left.p   -row 1 -column 0 -sticky swen
+	grid .left.avg -row 2 -column 0 -sticky swen
+	grid .left.med -row 3 -column 0 -sticky swen
     }
     setup_image {
 	showit

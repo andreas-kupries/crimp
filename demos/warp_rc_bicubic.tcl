7da9eabc19d2b5672e7729370d7650e558ea0600
Andreas Kupries <akupries@shaw.ca>
2010-11-14 00:26:35 +0000

    Extended the projective warp primitives with bicubic interpolation, snarfed from the matrix method.

---
+++ demos/warp_rc_bicubic.tcl
@@ -0,0 +1,36 @@
+def effect_warp_rc3_luma {
+    label {Warp/BiC luma (Rotate around center)}
+    setup {
+	variable angle -150
+
+	proc show {theangle} {
+	    variable cx
+	    variable cy
+	    variable i
+
+	    show_image [crimp warp projective -interpolate bicubic $i \
+			    [crimp transform rotate $theangle [list $cx $cy]]]
+	    return
+	}
+
+	proc showit {} {
+	    variable angle
+	    show $angle
+	    return
+	}
+
+	scale .left.s -variable ::DEMO::angle \
+	    -from -180 -to 180 -resolution 0.01 \
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	pack .left.s -side left -fill both -expand 1
+    }
+    setup_image {
+	variable i [crimp convert 2grey8 [base]]
+	variable cx [expr {[crimp width  $i]/2.}]
+	variable cy [expr {[crimp height $i]/2.}]
+
+	showit
+    }
+}

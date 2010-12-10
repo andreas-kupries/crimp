3c61a4d22fd18503f372a3c02e34d715c8912488
Andreas Kupries <akupries@shaw.ca>
2010-12-10 03:48:49 +0000

    Extended gradient demo with a canny edge-detector.

--- demos/gradiente.tcl
+++ demos/gradiente.tcl
@@ -6,6 +6,8 @@ def effect_gradient_simple {
     }
     setup {
 	variable greybase
+	variable low  25
+	variable high 75
 
 	proc row {args} {
 	    crimp montage horizontal {*}$args
@@ -21,6 +23,10 @@ def effect_gradient_simple {
 		0 0 255
 	}
 
+	proc e {i {n 1}} {
+	    crimp expand const $i $n $n $n $n
+	}
+
 	proc fg {i} {
 	    set s [crimp statistics basic $i]
 	    set min [dict get $s channel value min]
@@ -29,6 +35,16 @@ def effect_gradient_simple {
 	    set offset [expr {-1 * $min}]
 	    set scale  [expr {255.0/($max - $min)}]
 	    crimp convert 2grey8 [crimp::scale_float [crimp::offset_float $i $offset] $scale]
+	}
+
+	proc % {i} {
+	    set s [crimp statistics basic $i]
+	    set min [dict get $s channel value min]
+	    set max [dict get $s channel value max]
+	    # Compress (or expand) into the range 0...100 (percent).
+	    set offset [expr {-1 * $min}]
+	    set scale  [expr {100.0/($max - $min)}]
+	    return [crimp::scale_float [crimp::offset_float $i $offset] $scale]
 	}
 
 	proc rgb {i} {
@@ -45,26 +61,48 @@ def effect_gradient_simple {
 	}
 
 	proc GRADIENT {type} {
+	    variable no
+	    variable gc
+	    variable gp
+	    variable gv
 	    variable greybase
 	    set im [crimp convert 2float $greybase]
 	    set gc [crimp gradient $type  $im]
 	    set gp [crimp gradient polar  $gc]
 	    set gv [crimp gradient visual $gp]
 
+	    # Do canny edge detection...
 	    lassign $gc x y
 	    lassign $gp m a
+	    set ca [crimp::non_max_suppression [e $m] [e $a]]
+	    set no [% $ca]
 
-	    #log [crimp::type $x]
-	    #log [crimp::type $y]
-	    #log [crimp::type $m]
-	    #log [crimp::type $a]
+	    RETRACE
+	    return
+	}
 
+	proc RETRACE {args} {
+	    variable no
+	    variable gc
+	    variable gp
+	    variable gv
+	    variable greybase
+	    variable low
+	    variable high
+
+	    if {![info exists no]} return
+
+	    set ed [crimp::trace_hysteresis $no $low $high]
+
+	    lassign $gc x y
+	    lassign $gp m a
 	    show_image \
 		[stack \
 		     [row [b [base]]  [b $greybase]] \
 		     [row [b [fg $x]] [b [fg $y]]] \
 		     [row [b [fg $m]] [b [fg $a]]] \
-		     [row [b $gv]     [b [space $gv]]]]
+		     [row [b $gv]     [b $ed]]]
+	    return
 	}
 
 	proc SOBEL   {} { GRADIENT sobel   }
@@ -80,5 +118,18 @@ def effect_gradient_simple {
 	grid .top.prewi -row 0 -column 1 -sticky swen
 	grid .top.sobel -row 0 -column 2 -sticky swen
 	grid .top.schar -row 0 -column 3 -sticky swen
+
+	scale .left.l -variable ::DEMO::low \
+	    -from 0 -to 100 -resolution 0.1 \
+	    -orient vertical \
+	    -command ::DEMO::RETRACE
+
+	scale .left.h -variable ::DEMO::high \
+	    -from 0 -to 100 -resolution 0.1 \
+	    -orient vertical \
+	    -command ::DEMO::RETRACE
+
+	grid .left.l -row 0 -column 0 -sticky swen -rowspan 4
+	grid .left.h -row 0 -column 1 -sticky swen -rowspan 4
     }
 }

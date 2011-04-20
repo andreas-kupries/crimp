6e7b4a4c3150f6a465da579b81a325c31556b146
kennykb <kennykb>
2011-04-20 14:28:59 +0000

    simplify Gaussian derivative demos by using FITFLOAT

--- demos/gauss_gradient_mag_luma2.tcl
+++ demos/gauss_gradient_mag_luma2.tcl
@@ -1,23 +1,15 @@
 def op_gauss_gradient_mag_luma {
     label {Gauss IIR Gradient Magnitude Luma}
     setup {
-	variable sigma 0.2
+	variable sigma 1.5
 	variable table     {}
 
 	proc show {thesigma} {
 	    variable L
 	    if {![info exists L]} return
-	    set blurred	    [crimp::gaussian_gradient_mag_float \
-				$L $thesigma]
-	    set stats	    [crimp::stats_float $blurred]
-	    set offset	    [expr {- [dict get $stats value min]}]
-	    set range 	    [expr {[dict get $stats value max] 
-				   - [dict get $stats value min]}]
-	    set scale	    [expr {255.0 / $range}]
-	    set blurred	    [crimp::offset_float $blurred $offset]
-	    set blurred     [crimp::scale_float $blurred $scale]
-	    set blurred	    [crimp::convert 2grey8 $blurred]
-	    show_image	    $blurred
+	    show_image \
+		[crimp::FITFLOAT \
+		     [crimp::gaussian_gradient_mag_float $L $thesigma]]
 	    return
 	}
 

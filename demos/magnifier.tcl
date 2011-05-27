510c8b59304e7663f2b30721c7fe9e537c5ffaf9
Andreas Kupries <akupries@shaw.ca>
2011-05-27 00:27:57 +0000

    Extended the magnifier with multiple zoom settings, switchable via left/right mouse buttons.

--- demos/magnifier.tcl
+++ demos/magnifier.tcl
@@ -2,33 +2,76 @@ def op_magnifier {
     label Magnifier
     setup_image {
 	show_image [base]
+	replay
     }
     shutdown {
 	.c delete magnifier
 	crosshair::off .c
     }
     setup {
-	set G {}
 	set K [crimp kernel make {{0 1 1}} 1]
+
+	set settings {
+	    {1 64}
+	    {2 32}
+	    {3 16}
+	    {4  8}
+	    {5  4}
+	    {6  2}
+	    {0  0}
+	}
+
+	set current {0 0}
 
 	package require crosshair
 	crosshair::crosshair .c
 	crosshair::track on .c ::DEMO::track
+	bind .c <3> ::DEMO::next
+	bind .c <1> {::DEMO::next -1}
 
 	set mx 0
 	set my 0
 
+	set tx 0
+	set ty 0
+
 	.c create image [list $mx $my] -anchor center -tags magnifier
 	.c itemconfigure magnifier -image [image create photo]
 
+	proc next {{dir 1}} {
+	    variable current
+	    variable settings
+
+	    # current is at the end of settings, always.
+
+	    if {$dir > 0} {
+		set current  [lindex $settings 0]
+		set settings [linsert [lrange $settings 1 end] end $current]
+	    } else {
+		# end-1, because end is current.
+		set old $current
+		set current  [lindex $settings end-1]
+		set settings [linsert [lrange $settings 0 end-1] 0 $old]
+	    }
+
+	    log "zoom $current | $settings"
+	    replay
+	    return
+	}
+
 	proc get {i x y r} {
 	    # At x,y, block of radius r.
+
+	    #set trace {}
+	    #lappend trace "@ $x $y r$r"
 
 	    set w $r ; incr w $r
 	    set h $r ; incr h $r
 
 	    incr x -$r
 	    incr y -$r
+
+	    #lappend trace "rect $x $y ($w $h) [expr {$x+$w-1}] [expr {$y+$h-1}]"
 
 	    # Now the block is explicity specified as rectangle with
 	    # top-left corner at x,y and width, height.
@@ -41,6 +84,8 @@ def op_magnifier {
 
 	    set iw [crimp width  $i]
 	    set ih [crimp height $i]
+
+	    #lappend trace "versus $iw $ih"
 
 	    # Completely outside.
 	    if {($x >= $iw) ||
@@ -71,7 +116,11 @@ def op_magnifier {
 		incr h -$b
 	    }
 
-	    #puts "$x,$y ${w}x$h in ${iw}x${ih}"
+	    #lappend trace "blanks $l > ... < $r / $t V ... ^ $b"
+	    #lappend trace "cut $x,$y ${w}x$h in ${iw}x${ih}"
+
+	    #if {$w < 0} { puts [join $trace \n] ; exit }
+	    #if {$h < 0} { puts [join $trace \n] ; exit }
 
 	    # Cut (possibly shrunken) region
 	    set roi [crimp cut $i $x $y $w $h]
@@ -92,32 +141,62 @@ def op_magnifier {
 	}
 
 	proc track {_ x y xl yt xr yb} {
+	    variable current
 	    variable K
 	    variable mx
 	    variable my
-
-	    if {[base] eq {}} return
+	    variable tx
+	    variable ty
 
 	    set x [expr {int($x)}]
 	    set y [expr {int($y)}]
 
-	    # TODO? Put into text-item placed on top or beside (north)
-	    # the magnifier image overlay.
-	    log "@ $x $y"
+	    lassign $current scale radius
 
-	    # =============================================
-	    # each magnification step is 2x. 3 thus 8x
+	    .c lower magnifier
 
-	    crimp write 2tk [.c itemcget magnifier -image] \
-		[magnify 3 [get [base] $x $y 16]]
+	    # Remember last track position, for replay. Having this
+	    # separate from the magnifier location ensures that it
+	    # gets properly moved on replay, should it be necessary.
 
-	    # Move the magnifier on top of the crosshair.
+	    set tx $x
+	    set ty $y
+
+	    # (b) Do nothing without image or magnification disabled.
+	    if {([base] eq {}) ||
+		($scale  == 0) ||
+		($radius == 0)} {
+		return
+	    }
+
+	    # (a) Move the magnifier on top of the crosshair.
 	    set dx [expr {$x - $mx}]
 	    set dy [expr {$y - $my}]
 
-	    .c move magnifier $dx $dy
-	    set mx $x
-	    set my $y
+	    if {$dx || $dy} {
+		.c move magnifier $dx $dy
+		set mx $x
+		set my $y
+	    }
+
+	    # TODO? Put into text-item placed on top or beside (north)
+	    # the magnifier image overlay.
+	    set d [expr {2*$radius}]
+	    log "@ $x $y $scale $radius => [expr {2**$scale}]x\[${d}x${d}\]"
+
+	    # =============================================
+
+	    crimp write 2tk [.c itemcget magnifier -image] \
+		[magnify $scale [get [base] $x $y $radius]]
+
+	    .c raise magnifier
+	    return
+	}
+
+	proc replay {} {
+	    variable tx
+	    variable ty
+	    track _ $tx $ty _ _ _ _
 	}
     }
 }

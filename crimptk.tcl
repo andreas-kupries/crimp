81a8964b4e95045e3a8b8b65532cd36a92c8386f
Andreas Kupries <akupries@shaw.ca>
2011-06-29 06:04:30 +0000

    Moved the provide commands back to the end, with the latest critcl v3 changes the ordering trouble which caused their original move is gone.

--- crimptk.tcl
+++ crimptk.tcl
@@ -79,16 +79,15 @@ critcl::owns operator/*tk*.crimp
 }} [file dirname [file normalize [info script]]]
 
 # # ## ### ##### ######## #############
-
-package provide crimp::tk 0
-
-# # ## ### ##### ######## #############
 ## Make the C pieces ready. Immediate build of the binaries, no deferal.
 
 if {![critcl::load]} {
     error "Building and loading CRIMP::TK failed."
 }
 
+# # ## ### ##### ######## #############
+
+package provide crimp::tk 0
 return
 
 # vim: set sts=4 sw=4 tw=80 et ft=tcl:

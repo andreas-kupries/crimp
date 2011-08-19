29d4ecfdea91cf4507c3ddf4b28e6081e14d9be6
Andreas Kupries <akupries@shaw.ca>
2011-08-19 20:18:22 +0000

    Updated the PFM format handler to match the core's expectations.

--- policy_pfm.tcl
+++ policy_pfm.tcl
@@ -1,42 +1,54 @@
 # -*- tcl -*- 
-# # ## ### ##### ######## #############
-# Writing images as portable float map, in analogy to Portable Grey Maps, aka PGM.
-# See http://en.wikipedia.org/wiki/Netpbm_format
-# The type codes used for PFM are FAKE, i.e. made up.
+## Tcl level definitions for crimp::pfm.
+# # ## ### ##### ######## ############# #####################
 
-namespace eval ::crimp {}
+# # ## ### ##### ######## ############# #####################
 
-proc ::crimp::writes_pfm-plain_float {image} {
+## Writing images in the PFM format (Portable Float Map).
+## This is an unofficial derivative of PGM.
+## Reference
+##	 http://en.wikipedia.org/wiki/Netpbm_format
+
+# # ## ### ##### ######## ############# #####################
+## Reading PFM images is handled at the C level, and the command is
+## directly fit into the ::crimp::read ensemble.
+
+# # ## ### ##### ######## ############# #####################
+## Writing PFM images is handled by providing the command sets
+## expected by the standard 'write 2xxx' commands in the
+## "crimp::core".
+
+proc ::crimp::write::Str_pfm-plain_float {image} {
     # assert TypeOf (image) == float
-    set res "F2 [crimp dimensions $image]"
+    set res "F2 [::crimp dimensions $image]"
 
-    binary scan [crimp pixel $image] f* values
+    binary scan [::crimp pixel $image] f* values
     foreach v $values {
 	append res " " $v
     }
     return $res
 }
 
-proc ::crimp::writes_pfm-raw_float {image} {
+proc ::crimp::write::Str_pfm-raw_float {image} {
     # assert TypeOf (image) == float
-    return "F5 [crimp dimensions $image] [crimp pixel $image]"
+    return "F5 [::crimp dimensions $image] [::crimp pixel $image]"
 }
 
-proc ::crimp::writec_pfm-plain_float {chan image} {
+proc ::crimp::write::Chan_pfm-plain_float {chan image} {
     # assert TypeOf (image) == float
-    puts -nonewline $chan "F2 [crimp dimensions $image] "
+    puts -nonewline $chan "F2 [::crimp dimensions $image] "
 
-    binary scan [crimp pixel $image] f* values
+    binary scan [::crimp pixel $image] f* values
     foreach v $values {
 	puts -nonewline $chan " $v"
     }
     return
 }
 
-proc ::crimp::writec_pfm-raw_float {chan image} {
+proc ::crimp::write::Chan_pfm-raw_float {chan image} {
     # assert TypeOf (image) == float
-    puts -nonewline $chan "F5 [crimp dimensions $image] "
-    puts -nonewline $chan [crimp pixel $image]
+    puts -nonewline $chan "F5 [::crimp dimensions $image] "
+    puts -nonewline $chan [::crimp pixel $image]
     return
 }
 

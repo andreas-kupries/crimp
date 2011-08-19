cdd1eb72389478986d4751272f8cf687ad391d2c
Andreas Kupries <akupries@shaw.ca>
2011-08-19 05:06:13 +0000

    Updated the PPM format handler to match the core's expectations

--- policy_ppm.tcl
+++ policy_ppm.tcl
@@ -1,113 +1,130 @@
 # -*- tcl -*- 
-# # ## ### ##### ######## #############
-# Writing images in the PPM format (Portable Pix Map).
-# See http://en.wikipedia.org/wiki/Netpbm_format
+## Tcl level definitions for crimp::ppm.
+# # ## ### ##### ######## ############# #####################
+## Requirements
 
-namespace eval ::crimp {}
+package require crimp ; # Using "crimp convert xxx",
+                        #       "crimp::dimensions",
+                        #       "crimp::pixel"
 
-proc ::crimp::writes_ppm-plain_rgb {image} {
+# # ## ### ##### ######## ############# #####################
+
+## Writing images in the PPM format (Portable Pix Map).
+## Reference
+##	http://en.wikipedia.org/wiki/Netpbm_format
+
+# # ## ### ##### ######## ############# #####################
+## Reading PPM images is handled at the C level, and the command is
+## directly fit into the ::crimp::read ensemble.
+
+# # ## ### ##### ######## ############# #####################
+## Writing PPM images is handled by providing the command sets
+## expected by the standard 'write 2xxx' commands in the
+## "crimp::core".
+
+proc ::crimp::write::Str_ppm-plain_rgb {image} {
     # assert TypeOf (image) == rgb
-    set res "P3 [crimp dimensions $image] 255"
-    foreach c [::split [crimp pixel $image] {}] {
+    set res "P3 [::crimp dimensions $image] 255"
+    foreach c [::split [::crimp pixel $image] {}] {
 	binary scan $c cu g
 	append res " " $g
     }
     return $res
 }
 
-proc ::crimp::writes_ppm-raw_rgb {image} {
+proc ::crimp::write::Str_ppm-raw_rgb {image} {
     # assert TypeOf (image) == rgb
-    return "P6 [crimp dimensions $image] 255 [crimp pixel $image]"
+    return "P6 [::crimp dimensions $image] 255 [::crimp pixel $image]"
 }
 
-proc ::crimp::writec_ppm-plain_rgb {chan image} {
+proc ::crimp::write::Chan_ppm-plain_rgb {chan image} {
     # assert TypeOf (image) == rgb
-    puts -nonewline $chan "P3 [crimp dimensions $image] 255"
-    foreach c [::split [crimp pixel $image] {}] {
+    puts -nonewline $chan "P3 [::crimp dimensions $image] 255"
+    foreach c [::split [::crimp pixel $image] {}] {
 	binary scan $c cu g
 	puts -nonewline $chan " $g"
     }
     return
 }
 
-proc ::crimp::writec_ppm-raw_rgb {chan image} {
+proc ::crimp::write::Chan_ppm-raw_rgb {chan image} {
     # assert TypeOf (image) == rgb
-    puts -nonewline $chan "P6 [crimp dimensions $image] 255 "
-    puts -nonewline $chan [crimp pixel $image]
+    puts -nonewline $chan "P6 [::crimp dimensions $image] 255 "
+    puts -nonewline $chan [::crimp pixel $image]
     return
 }
 
-# # ## ### ##### ######## #############
+# # ## ### ##### ######## ############# #####################
 
-proc ::crimp::writes_ppm-plain_rgba {image} {
+proc ::crimp::write::Str_ppm-plain_rgba {image} {
     # assert TypeOf (image) == rgba
-    return [writes_ppm-plain_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-plain_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writes_ppm-raw_rgba {image} {
+proc ::crimp::write::Str_ppm-raw_rgba {image} {
     # assert TypeOf (image) == rgba
-    return [writes_ppm-raw_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-raw_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writec_ppm-plain_rgba {chan image} {
+proc ::crimp::write::Chan_ppm-plain_rgba {chan image} {
     # assert TypeOf (image) == rgba
-    writec_ppm-plain_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-plain_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-proc ::crimp::writec_ppm-raw_rgba {chan image} {
+proc ::crimp::write::Chan_ppm-raw_rgba {chan image} {
     # assert TypeOf (image) == rgba
-    writec_ppm-raw_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-raw_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-# # ## ### ##### ######## #############
+# # ## ### ##### ######## ############# #####################
 
-proc ::crimp::writes_ppm-plain_hsv {image} {
+proc ::crimp::write::Str_ppm-plain_hsv {image} {
     # assert TypeOf (image) == hsv
-    return [writes_ppm-plain_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-plain_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writes_ppm-raw_hsv {image} {
+proc ::crimp::write::Str_ppm-raw_hsv {image} {
     # assert TypeOf (image) == hsv
-    return [writes_ppm-raw_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-raw_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writec_ppm-plain_hsv {chan image} {
+proc ::crimp::write::Chan_ppm-plain_hsv {chan image} {
     # assert TypeOf (image) == hsv
-    writec_ppm-plain_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-plain_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-proc ::crimp::writec_ppm-raw_hsv {chan image} {
+proc ::crimp::write::Chan_ppm-raw_hsv {chan image} {
     # assert TypeOf (image) == hsv
-    writec_ppm-raw_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-raw_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-# # ## ### ##### ######## #############
+# # ## ### ##### ######## ############# #####################
 
-proc ::crimp::writes_ppm-plain_grey8 {image} {
+proc ::crimp::write::Str_ppm-plain_grey8 {image} {
     # assert TypeOf (image) == grey8
-    return [writes_ppm-plain_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-plain_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writes_ppm-raw_grey8 {image} {
+proc ::crimp::write::Str_ppm-raw_grey8 {image} {
     # assert TypeOf (image) == grey8
-    return [writes_ppm-raw_rgb [crimp convert 2rgb $image]]
+    return [Str_ppm-raw_rgb [::crimp convert 2rgb $image]]
 }
 
-proc ::crimp::writec_ppm-plain_grey8 {chan image} {
+proc ::crimp::write::Chan_ppm-plain_grey8 {chan image} {
     # assert TypeOf (image) == grey8
-    writec_ppm-plain_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-plain_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-proc ::crimp::writec_ppm-raw_grey8 {chan image} {
+proc ::crimp::write::Chan_ppm-raw_grey8 {chan image} {
     # assert TypeOf (image) == grey8
-    writec_ppm-raw_rgb $chan [crimp convert 2rgb $image]
+    Chan_ppm-raw_rgb $chan [::crimp convert 2rgb $image]
     return
 }
 
-# # ## ### ##### ######## #############
+# # ## ### ##### ######## ############# #####################
 return

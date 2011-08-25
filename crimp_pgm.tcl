e814c36e5b5684ccafd6b94838314a45ff9199fa
Andreas Kupries <akupries@shaw.ca>
2011-08-25 18:27:35 +0000

    Fixed missing ownership declaration for the build-time support file, ensuring its presence in TEA-wraps.

--- crimp_pgm.tcl
+++ crimp_pgm.tcl
@@ -1,20 +1,18 @@
 # -*- tcl -*-
-# CRIMP :: Tk == C Runtime Image Manipulation Package :: Tk Photo Conversion.
+# CRIMP :: PGM // Portable GreyMap // Import, Export
 #
-# (c) 2010      Andrew M. Goth  http://wiki.tcl.tk/andy%20goth
-# (c) 2010-2011 Andreas Kupries http://wiki.tcl.tk/andreas%20kupries
+# (c) 2011 Andreas Kupries http://wiki.tcl.tk/andreas%20kupries
 #
 
 # # ## ### ##### ######## #############
 ## Requisites
 
-package require critcl       3
-package require critcl::util 1
+package require critcl 3
 
 # # ## ### ##### ######## #############
 
 if {![critcl::compiling]} {
-    error "Unable to build CRIMP::TK, no proper compiler found."
+    error "Unable to build CRIMP::PGM, no proper compiler found."
 }
 
 # # ## ### ##### ######## #############
@@ -35,35 +33,28 @@ critcl::license \
     {Under a BSD license.}
 
 critcl::summary \
-    {Extension of the CRIMP core to handle import and export of Tk photos}
+    {Extension of the CRIMP core to handle import and export of PGM images}
 
 critcl::description {
     This package provides the CRIMP eco-system with the functionality to handle
-    images stored in Tk photo images. This means that this package is dependent
-    on Tk, and thus the presence of a windowing system, like X11, or Aqua.
+    images stored as Portable GreyMap (PGM). It can read and write both plain
+    and raw formats.
 }
 
-critcl::subject image {Tk photo image} {Tk photo} {Tk photo import} {Tk photo export}
+critcl::subject image {PGM image} {Portable GreyMap} {PGM import} {PGM export}
 critcl::subject image {image import} {image export}
-critcl::subject photo {photo import} {photo export}
 
 # # ## ### ##### ######## #############
 ## Implementation.
 
 critcl::tcl 8.5
-critcl::tk
 
 # # ## ### ##### ######## #############
 ## Declare the Tcl layer aggregating the C primitives into useful
 ## commands. After the Tcl-based readers and writers to properly pick
 ## them up too in the ensembles.
 
-critcl::tsources policy_tk.tcl
-
-# # ## ### ##### ######## #############
-## Chart helpers.
-
-critcl::tsources plot.tcl
+critcl::tsources policy_pgm.tcl
 
 # # ## ### ##### ######## #############
 ## C-level API (i.e. stubs and types)
@@ -80,21 +71,21 @@ critcl::ccode {
 }
 
 # # ## ### ##### ######## #############
-## Pull in the Tk-specific pieces. These are fit under the read/write namespaces.
+## Pull in the Pgm-specific pieces. These are fit under the read/write namespaces.
 
-critcl::owns        format/*tk*.crimp
-crimp_source_cproc {format/*tk*.crimp}
+critcl::owns        format/*pgm*.crimp
+crimp_source_cproc {format/*pgm*.crimp}
 
 # # ## ### ##### ######## #############
 ## Make the C pieces ready. Immediate build of the binaries, no deferal.
 
 if {![critcl::load]} {
-    error "Building and loading CRIMP::TK failed."
+    error "Building and loading CRIMP::PGM failed."
 }
 
 # # ## ### ##### ######## #############
 
-package provide crimp::tk 0.1
+package provide crimp::pgm 0.1
 return
 
 # vim: set sts=4 sw=4 tw=80 et ft=tcl:

21037a44cc48eaf33011b5ae51f57528bf194eae
Andreas Kupries <akupries@shaw.ca>
2011-08-26 22:37:24 +0000

    Same treatment to the gaussian noise generator. The bug with the two dependent random variables is still present at this point.

--- demos/noise_gaussian.tcl
+++ demos/noise_gaussian.tcl
@@ -1,42 +1,34 @@
 def op_Noise_Gaussian {
-    label {NOISE GAUSSIAN }
+    label {Noise - Gaussian}
     active {
 	expr {[bases] == 1}
     }
     setup_image {
-	variable L  [base]
 	show
     }
     setup {
 	variable mean      0
 	variable variance  0.005
-	
-	
-	
-	proc show {args} {
-	    variable L
-        variable mean      
-	    variable variance  
-		
 
-	   show_image    [crimp noise gaussian $L  $mean $variance ]  
-	        return
+	proc show {args} {
+	    variable mean
+	    variable variance
+
+	    show_image [crimp noise gaussian [base] $mean $variance ]
+	    return
 	}
 
 	scale .left.mean -variable ::DEMO::mean \
 	    -from 0 -to 1 -resolution .005 -length 400\
 	    -orient vertical \
 	    -command ::DEMO::show
-		
-    scale .left.variance -variable ::DEMO::variance \
+
+	scale .left.variance -variable ::DEMO::variance \
 	    -from 0 -to 1 -resolution .005 -length 400\
 	    -orient vertical \
 	    -command ::DEMO::show
-		
 
 	grid .left.mean     -row 0 -column 0 -sticky swen
 	grid .left.variance -row 0 -column 1 -sticky swen
-    
-
     }
 }

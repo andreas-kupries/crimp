ad591ce40a460b681596e872dadd57a0383f9d13
Andreas Kupries <akupries@shaw.ca>
2011-08-28 02:46:26 +0000

    Fixing up translation-only registration. Only bug was that the delta values had no post-processing to make them properly signed. Simplified the setup, the changes in [bce7e722de] now allow for a single-step conversion to complex-typed images. Simplified post-processing using the extended statistics of [8b14cc98cc] to get the peak location directly. Extended setup with application of the windowing. Added derivate of Kevin's demo showing off translation-only registration with all the internals visible.

--- demos/Registration_translation.tcl
+++ demos/Registration_translation.tcl
@@ -10,36 +10,36 @@ def op_Registration_translation {
 	show
     }
     setup {
-		
+	
 	
 	proc show {args} {
 	    
-		variable  image1
-        variable  xshift
-        
-		set tranrotate  [::crimp::transform rotate [expr {360 - $xshift } ] ] 
-        set image2      [::crimp::warp::projective $image1 $tranrotate]
-		
-        set lpt1 [crimp::transform::logpolar $image1 360 360 ]
-        set lpt2 [crimp::transform::logpolar $image2 360 360 ]
-        
+	    variable  image1
+	    variable  xshift
+	    
+	    set tranrotate  [::crimp::transform rotate [expr {360 - $xshift } ] ] 
+	    set image2      [::crimp::warp::projective $image1 $tranrotate]
+	    
+	    set lpt1 [crimp::transform::logpolar $image1 360 360 ]
+	    set lpt2 [crimp::transform::logpolar $image2 360 360 ]
+	    
 
-	
-		set statstrans [::crimp::imregs::translation $lpt1 $lpt2 ]
+	    
+	    set statstrans [::crimp::imregs::translation $lpt1 $lpt2 ]
 	    log "\n\n"
 	    log $statstrans
-		set disp [::crimp::montage::horizontal $lpt1 $lpt2 ]
-           show_image     $disp
-	        return
+	    set disp [::crimp::montage::horizontal $lpt1 $lpt2 ]
+	    show_image     $disp
+	    return
 	}
 
 	scale .left.xshift -variable ::DEMO::xshift \
 	    -from 0 -to 360 -resolution 10 -length 360\
 	    -orient vertical \
 	    -command ::DEMO::show
-		
+	
    	grid .left.xshift     -row 0 -column 0 -sticky swen
-		
+	
 	
     }
 }

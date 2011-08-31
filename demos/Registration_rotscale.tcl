257a31ba096fd9dc77f3a8156d45b8612c037845
Andreas Kupries <akupries@shaw.ca>
2011-08-31 20:24:20 +0000

    More reformatting to style, tweaks to variable names for clarity in some places. Plus one new demo for the checker board image.

--- demos/Registration_rotscale.tcl
+++ demos/Registration_rotscale.tcl
@@ -4,34 +4,29 @@ def op_Registration_rotscale {
 	expr {[bases] == 1}
     }
     setup_image {
-	variable  image1 [base] 
+	variable image1 [base] 
 	variable angle 90
 	variable scale 0.4
-	
-	
+
 	show
     }
     setup {
-		
-	
 	proc show {args} {
+	    variable  image1
+	    variable  angle
+	    variable  scale
 	    
-		variable  image1
-        variable  angle
-        variable  scale
-		
-		set transcale   [::crimp::transform scale $scale $scale ] 
-        set tranrotate  [::crimp::transform rotate $angle ]
-        set transform   [::crimp::transform::chain $transcale $tranrotate ]
-        set image2      [::crimp::warp::projective $image1 $transform]
-		
-			
-		set statstrans [::crimp::register::rotscale $image1 $image2 ]
+	    set transcale   [::crimp::transform scale $scale $scale ] 
+	    set tranrotate  [::crimp::transform rotate $angle ]
+	    set transform   [::crimp::transform::chain $transcale $tranrotate ]
+	    set image2      [::crimp::warp::projective $image1 $transform]
+	    
+	    set statstrans [::crimp::register::rotscale $image1 $image2 ]
 	    log "\n\n"
 	    log $statstrans
-		set disp [::crimp::montage::horizontal $image1 $image2 ]
-           show_image     $disp
-	        return
+	    set disp [::crimp::montage::horizontal $image1 $image2 ]
+	    show_image     $disp
+	    return
 	}
 
 	scale .left.angle -variable ::DEMO::angle \
@@ -43,10 +38,8 @@ def op_Registration_rotscale {
 	    -from 0 -to 2 -resolution .2 -length 10\
 	    -orient vertical \
 	    -command ::DEMO::show
-		
+	
    	grid .left.angle     -row 0 -column 0 -sticky swen
 	grid .left.scale     -row 0 -column 1 -sticky swen
-		
-	
     }
 }
--- demos/canny_sobel.tcl
+++ demos/canny_sobel.tcl
@@ -1,27 +1,20 @@
 def op_Edge_Canny_Sobel {
-    label {Edge Canny Sobel }
-    active {
-	expr {[bases] == 1}
-    }
+    label {Edge Canny Sobel}
     setup_image {
-	variable L [base]
 	show
     }
     setup {
 	variable high  100
 	variable low    50
 	variable sigma   1
-		
+	
 	proc show {args} {
-	    variable L
-
 	    variable high
 	    variable low
 	    variable sigma
-        
 
-	   show_image     [crimp filter canny sobel $L $high $low $sigma ]
-	        return
+	    show_image [crimp filter canny sobel [base] $high $low $sigma]
+	    return
 	}
 
 	scale .left.high -variable ::DEMO::high \
@@ -35,17 +28,12 @@ def op_Edge_Canny_Sobel {
 	    -command ::DEMO::show
 
 	scale .left.sigma -variable ::DEMO::sigma \
-	    -from 0.1 -to 5 -resolution 0.1 -length 50\
+	    -from 0.1 -to 5 -resolution 0.1 -length 100 \
 	    -orient vertical \
 	    -command ::DEMO::show
 
-	
-
-	grid .left.high -row 0 -column 0 -sticky swen
-	grid .left.low  -row 0 -column 1 -sticky swen
-	
-	grid .left.sigma  -row 0 -column 2 -sticky swen
-    
-
+	grid .left.high  -row 0 -column 0 -sticky swen
+	grid .left.low   -row 0 -column 1 -sticky swen
+	grid .left.sigma -row 0 -column 2 -sticky swen
     }
 }

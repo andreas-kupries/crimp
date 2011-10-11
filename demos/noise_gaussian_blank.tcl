72047ab9fe0909a45bd685586071ba7c2d989cd8
Andreas Kupries <akupries@shaw.ca>
2011-10-11 21:47:52 +0000

    Added two demos of gaussian noise with synthetic images as input, no images to select.

---
+++ demos/noise_gaussian_blank.tcl
@@ -0,0 +1,42 @@
+def op_Noise_Gaussian_Blank {
+    label {Noise - Gaussian / Blank}
+    active {
+	expr {[bases] == 0}
+    }
+    setup_image {
+	show
+    }
+    setup {
+	variable mean      0
+	variable variance  0.005
+	variable base      [crimp blank grey8 5 5 128]
+
+	set K [crimp kernel make {{0 1 1}} 1]
+	proc 8x {image} {
+	    variable K
+	    return [crimp interpolate xy [crimp interpolate xy [crimp interpolate xy $image 2 $K] 2 $K] 2 $K]
+	}
+
+	proc show {args} {
+	    variable mean
+	    variable variance
+	    variable base
+
+	    show_image [8x [8x [crimp noise gaussian $base $mean $variance]]]
+	    return
+	}
+
+	scale .left.mean -variable ::DEMO::mean \
+	    -from 0 -to 1 -resolution .005 -length 400\
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	scale .left.variance -variable ::DEMO::variance \
+	    -from 0 -to 1 -resolution .005 -length 400\
+	    -orient vertical \
+	    -command ::DEMO::show
+
+	grid .left.mean     -row 0 -column 0 -sticky swen
+	grid .left.variance -row 0 -column 1 -sticky swen
+    }
+}

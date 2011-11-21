bddb47d932e3a8b3bc459186006ad7a1f4e3d905
Andreas Kupries <akupries@shaw.ca>
2011-11-21 18:00:46 +0000

    Created a code-generator for the C code of binary operations, with a template. The new code takes the full image gemeotry into account when performing the operation, with the result having the bounding box of the union of the input geometries as its geometry. No checks for image size mismatch anymore. Note: When present alpha channels are combined via MAX, not the chosen operation. When mixing images with and without alpha channel the image without is treated as having a virtual alpha channel which is fully opaque.

---
+++ cop/binop_template.c
@@ -0,0 +1,63 @@
+crimp_image*     result;
+crimp_image*     imageA;
+crimp_image*     imageB;
+int px, py, lx, ly, oxa, oya, oxb, oyb;
+crimp_geometry bb;
+
+crimp_input (imageAObj, imageA, @TYPE_A@);
+crimp_input (imageBObj, imageB, @TYPE_B@);
+
+/*
+ * Compute union area of the two images to process.
+ * Note how the images do not have to match in size.
+ */
+
+crimp_rect_union (&imageA->geo, &imageB->geo, &bb);
+
+result = crimp_new_@TYPE_Z@_at (bb.x, bb.y, bb.w, bb.h);
+oxa = crimp_x (imageA);
+oya = crimp_y (imageA);
+oxb = crimp_x (imageB);
+oyb = crimp_y (imageB);
+
+/*
+ * x, y are physical coordinates in the result, starting from 0.
+ * The associated logical coordinates in the 2D plane are
+ *  lx = px + x(result)
+ *  lx = py + y(result)
+ * And when we are inside an input its physical coordinates, from the logical are
+ *  px = lx - x(input)
+ *  py = ly - y(input)
+ */
+
+for (py = 0; py < bb.h; py++) {
+    for (px = 0; px < bb.w; px++) {
+
+        int lx = px + bb.x;
+        int ly = py + bb.y;
+
+	int ina = crimp_inside (imageA, lx, ly);
+	int inb = crimp_inside (imageB, lx, ly);
+
+	/*
+	 * The result depends on where we are relative to both input.
+	 * Inside of each input we take the respective value of the
+	 * pixel. Outside of an input we take BLACK as the value
+	 * instead.
+	 */
+
+@TRANSFORM@
+    }
+}
+
+Tcl_SetObjResult(interp, crimp_new_image_obj (result));
+return TCL_OK;
+
+/* vim: set sts=4 sw=4 tw=80 et ft=c: */
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * fill-column: 78
+ * End:
+ */

e202d50c462aae6067f4d09eae391ca7dde12bfc
Andreas Kupries <akupries@shaw.ca>
2012-06-19 23:12:42 +0000

    Tweaked testsuite driver to run files in alphabetical order.

--- test/all.tcl
+++ test/all.tcl
@@ -2,7 +2,6 @@
 # Run all .test files in this file's directory, i.e.
 # the .test siblings of this file.
 
-foreach t [glob -directory [file dirname [file normalize [info script]]] \
-	       *.test] {
+foreach t [lsort -dict [glob -directory [file dirname [file normalize [info script]]] *.test]] {
     source $t
 }

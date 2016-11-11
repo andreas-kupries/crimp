829791433aad00416037e38d4b674735a36d9ebf
Andreas Kupries <akupries@shaw.ca>
2016-11-11 20:37:16 +0000

    Added compile time sizeof information, used where compiler complained (for now).

--- c/cutil.h
+++ c/cutil.h
@@ -29,11 +29,48 @@
 #define MINVAL        (0)
 #define MAXVAL_GREY8  (255)
 #define MAXVAL_GREY16 (65535)
-#define MAXVAL_GREY32 (4294967295)
+#define MAXVAL_GREY32 (4294967295UL)
 
 #define MAXVAL_STR_GREY8  "255"
 #define MAXVAL_STR_GREY16 "65535"
 #define MAXVAL_STR_GREY32 "4294967295"
+
+/*
+ * compile-time sizeof calculations, via <limits.h>
+ *
+ * TODO: Look for <stdint.h>, create backward portable replacement for machine/compile 
+ *       combinations which do not provide it.
+ */
+
+#include <limits.h>
+
+#if UINT_MAX == 18446744073709551615UL /* 64-bit int */
+#define CRIMP_LONG_SIZE 8
+#elif UINT_MAX == MAXVAL_GREY32        /* 32-bit int */
+#define CRIMP_INT_SIZE 4
+#elif UINT_MAX == MAXVAL_GREY16        /* 16-bit int */
+#define CRIMP_INT_SIZE 2
+#elif UINT_MAX == MAXVAL_GREY8         /*  8-bit int */
+#define CRIMP_INT_SIZE 1
+#else
+#error "Unable to determine sizeof(int)"
+#endif
+
+#if ULONG_MAX == 18446744073709551615UL /* 64-bit long */
+#define CRIMP_LONG_SIZE 8
+#elif ULONG_MAX == MAXVAL_GREY32        /* 32-bit long */
+#define CRIMP_LONG_SIZE 4
+#elif ULONG_MAX == MAXVAL_GREY16        /* 16-bit long */
+#define CRIMP_LONG_SIZE 2
+#elif ULONG_MAX == MAXVAL_GREY8         /*  8-bit long */
+#define CRIMP_LONG_SIZE 1
+#else
+#error "Unable to determine sizeof(long)"
+#endif
+
+/*
+ * Miscellaneous constants and machine-specific settings.
+ */
 
 #ifndef M_PI
 #define M_PI (3.141592653589793238462643)
